01ba592c9fed526711dcdd13a5e15f0e445c9e74
shreeyansh64 <shreeyansh64@gmail.com>
2025-10-01 04:29:05 +0530

    Added buttons in NotificationsPage

--- lib/pages/notificationspage.dart
+++ lib/pages/notificationspage.dart
@@ -23,8 +23,94 @@ class _NotificationsPageState extends State<NotificationsPage> {
           ),
         ),
         actions: [
-          Icon(Icons.more_vert,color: Color.fromARGB(255, 142, 175, 222))
+          Icon(Icons.more_vert, color: Color.fromARGB(255, 142, 175, 222)),
         ],
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Color.fromARGB(255, 23, 24, 28),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 3.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 48, 48, 56),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "Inbox",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 242, 243, 247),
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Icon(
+                      Icons.arrow_drop_down,
+                      color: Color.fromARGB(255, 242, 243, 247),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 3.0, left: 3.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 48, 48, 56),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                ),
+                child: Text(
+                  "Focused",
+                  style: TextStyle(color: Color.fromARGB(255, 242, 243, 247)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 3.0, left: 3.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 48, 48, 56),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                ),
+                child: Text(
+                  "Unread",
+                  style: TextStyle(color: Color.fromARGB(255, 242, 243, 247)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 3.0),
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 48, 48, 56),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      "Repository",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 242, 243, 247),
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Icon(
+                      Icons.arrow_drop_down,
+                      color: Color.fromARGB(255, 242, 243, 247),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
